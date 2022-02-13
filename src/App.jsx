4ccd33c6aa096547009b33a1525f13dfeceb8da2
Pranav <pranavmendi@gmail.com>
2022-02-14 03:48:07 +0530

    feat: add header and main navigation component
    Add header and main navigation component, setup routes in navigation using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import LabelBottomNavigation from "./components/MainNav";
-import { Container, Switch } from "@mui/material";
+import { Container } from "@mui/material";
+import Trending from "./Pages/Trending/Trending";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Search from "./Pages/Search/Search";
 
 function App() {
   return (
@@ -12,12 +16,12 @@ function App() {
         <Header />
         <div className="app">
           <Container>
-            <Switch>
-              <Route path="/" component={Trending} exact />
-              <Route path="/movies" component={Movies} />
-              <Route path="/series" component={Series} />
-              <Route path="/search" component={Search} />
-            </Switch>
+            <Routes>
+              <Route exact path="/" element={<Trending />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/series" element={<Series />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
           </Container>
         </div>
         <LabelBottomNavigation />
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,10 +1,24 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import { MovieCreation, Search, Tv, Whatshot } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState("recents");
+  const [value, setValue] = useState(0);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (value === 0) {
+      navigate("/");
+    } else if (value === 1) {
+      navigate("/movies");
+    } else if (value === 2) {
+      navigate("/series");
+    } else if (value === 3) {
+      navigate("/search");
+    }
+  }, [value, navigate]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -25,25 +39,21 @@ export default function LabelBottomNavigation() {
       <BottomNavigationAction
         sx={{ color: "white" }}
         label="Trending"
-        value="trending"
         icon={<Whatshot />}
       />
       <BottomNavigationAction
         sx={{ color: "white" }}
         label="Movies"
-        value="Movies"
         icon={<MovieCreation />}
       />
       <BottomNavigationAction
         sx={{ color: "white" }}
         label="TV-Series"
-        value="TV-Series"
         icon={<Tv />}
       />
       <BottomNavigationAction
         sx={{ color: "white" }}
         label="Search"
-        value="Search"
         icon={<Search />}
       />
     </BottomNavigation>
