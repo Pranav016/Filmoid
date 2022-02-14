6b1e40f0c1bb1cc905f8465e12966b088e1ec674
Pranav <pranavmendi@gmail.com>
2022-02-14 10:35:14 +0530

    style: add styles to App, Trending page and single movie component, update eslint config

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <span className="header">Filmoid</span>;
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+      Filmoid
+    </span>
+  );
 };
 
 export default Header;
